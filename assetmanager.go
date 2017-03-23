b1d3f6b4fd8287b29b19a4747dfa7503d680a83f
s4l1h <kingemkurdam@gmail.com>
2017-03-24 01:53:24 +0300

    init

---
+++ assetmanager.go
@@ -0,0 +1,199 @@
+package assetmanager
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+// ReplaceFunc file name replacer function
+type ReplaceFunc func(name string) string
+
+//AssetManager asset manager
+type AssetManager struct {
+	Files         map[string][]byte
+	AllowedExt    map[string]bool
+	DisallowedExt map[string]bool
+	Replacers     map[string]ReplaceFunc
+}
+
+// New new asset manager
+func New() *AssetManager {
+	return &AssetManager{
+		Files:         make(map[string][]byte),
+		AllowedExt:    make(map[string]bool),
+		DisallowedExt: make(map[string]bool),
+		Replacers:     make(map[string]ReplaceFunc),
+	}
+}
+
+// AddReplacer add replacer function
+func (manager *AssetManager) AddReplacer(name string, fn ReplaceFunc) {
+	manager.Replacers[name] = fn
+}
+
+// RemoveReplacer remove replacer function
+func (manager *AssetManager) RemoveReplacer(name string) {
+	delete(manager.Replacers, name)
+}
+
+// ExistsReplacer check replacer function exists
+func (manager *AssetManager) ExistsReplacer(name string) bool {
+	_, ok := manager.Replacers[name]
+	return ok
+}
+
+// AddAllowedExt add allowed extension
+func (manager *AssetManager) AddAllowedExt(ext ...string) {
+	for _, e := range ext {
+		manager.AllowedExt[e] = true
+	}
+}
+
+// RemoveAllowedExt remove allowed extension
+func (manager *AssetManager) RemoveAllowedExt(ext string) {
+	delete(manager.AllowedExt, ext)
+}
+
+// AddDisallowExt add disallow extension
+func (manager *AssetManager) AddDisallowExt(ext ...string) {
+	for _, e := range ext {
+		manager.DisallowedExt[e] = true
+	}
+}
+
+// RemoveDisallowExt remove disallow extension
+func (manager *AssetManager) RemoveDisallowExt(ext string) {
+	delete(manager.DisallowedExt, ext)
+}
+
+// CheckAllowed check file allowed and trusted file.
+func (manager *AssetManager) CheckAllowed(ext string) bool {
+	if len(manager.AllowedExt) == 0 {
+		return true
+	}
+	if _, ok := manager.AllowedExt[ext]; ok {
+		return true
+	}
+	return false
+}
+
+// CheckDisallowed check file disallowed and trusted file.
+func (manager *AssetManager) CheckDisallowed(ext string) bool {
+	if len(manager.DisallowedExt) == 0 {
+		return true
+	}
+	if _, ok := manager.DisallowedExt[ext]; ok {
+		return false
+	}
+	return true
+}
+
+// GetExt return file extension
+func (manager *AssetManager) GetExt(file string) string {
+	return filepath.Ext(file)
+}
+
+// CheckAddFile check file.
+func (manager *AssetManager) CheckAddFile(file string) bool {
+	ext := manager.GetExt(file)
+	if manager.CheckAllowed(ext) && manager.CheckDisallowed(ext) {
+		return true
+	}
+	return false
+}
+
+// AddFile add file
+func (manager *AssetManager) AddFile(files ...string) {
+	for _, file := range files {
+		//logrus.Warn(file)
+		if !manager.CheckAddFile(file) {
+			continue
+		}
+		if b, er := ioutil.ReadFile(file); er == nil {
+			manager.add(file, b)
+			//manager.Files[file] = b
+		}
+	}
+}
+
+// AddFileString add file
+func (manager *AssetManager) AddFileString(name, content string) {
+	if manager.CheckAddFile(name) {
+		manager.add(name, []byte(content))
+		//manager.Files[name] = []byte(content)
+	}
+}
+
+func (manager *AssetManager) add(name string, content []byte) {
+	//logrus.Warn(len(manager.Replacers))
+	if len(manager.Replacers) != 0 {
+		for _, replacer := range manager.Replacers {
+			//logrus.Warnf("%T", replacer)
+			name = replacer(name)
+		}
+	}
+	manager.Files[name] = content
+}
+
+// AddDir add directories
+func (manager *AssetManager) AddDir(dirs ...string) {
+	fileList := []string{}
+	for _, dir := range dirs {
+		//logrus.Warn("Add Dir", dir)
+		filepath.Walk(dir, func(path string, f os.FileInfo, err error) error {
+			if !f.IsDir() {
+				//logrus.Warnf("Add File %s ", path)
+				fileList = append(fileList, path)
+			}
+			return nil
+		})
+	}
+
+	manager.AddFile(fileList...)
+	//logrus.Warn(fileList)
+}
+
+// Get get file
+func (manager *AssetManager) Get(file string) ([]byte, error) {
+	if val, ok := manager.Files[file]; ok {
+		return val, nil
+	}
+	return nil, fmt.Errorf("File Not Found %s", file)
+}
+
+// GetString get file string
+func (manager *AssetManager) GetString(file string) (string, error) {
+	content, err := manager.Get(file)
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
+}
+
+// Exists check file exists
+func (manager *AssetManager) Exists(file string) bool {
+	_, ok := manager.Files[file]
+	return ok
+}
+
+// Merge another assetmanager
+func (manager *AssetManager) Merge(asset *AssetManager) *AssetManager {
+	if len(asset.Files) != 0 {
+		for name, file := range asset.Files {
+			manager.Files[name] = file
+		}
+	}
+	return manager
+}
+
+// MergeAndRunReplacer another assetmanager
+func (manager *AssetManager) MergeAndRunReplacer(asset *AssetManager) *AssetManager {
+	if len(asset.Files) != 0 {
+		for name, file := range asset.Files {
+			manager.add(name, file)
+		}
+	}
+	return manager
+}
---
+++ assetmanager_test.go
@@ -0,0 +1,225 @@
+package assetmanager_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/akmyazilim/assetmanager"
+)
+
+func ExampleAssetManager_AddReplacer() {
+	asset := assetmanager.New()
+	// Add Replacer
+	asset.AddReplacer("testReplacer", func(name string) string {
+		return fmt.Sprintf("main:%s", name)
+	})
+	// Add Another Replacer.
+	asset.AddReplacer("convergohtml", func(name string) string {
+		return strings.Replace(name, ".html", ".gohtml", -1)
+	})
+	// add file from string
+	asset.AddFileString("index.html", "test content")
+	// get file content
+	c, _ := asset.GetString("main:index.gohtml")
+	fmt.Println(c)
+	// Output: test content
+}
+func replaceMe(name string) string {
+	return name
+}
+
+func ExampleAssetManager_Merge() {
+	asset := assetmanager.New()
+	asset.AddFileString("index.html", "index.html content")
+	asset.AddFileString("asset1.html", "asset1.html content")
+	asset2 := assetmanager.New()
+	asset2.AddFileString("asset2.html", "asset2.html content")
+	asset.Merge(asset2)
+	c, _ := asset.GetString("asset2.html")
+	fmt.Println(c)
+	// Output: asset2.html content
+}
+func ExampleAssetManager_MergeAndRunReplacer() {
+	asset := assetmanager.New()
+	asset.AddReplacer("ToUpper", func(name string) string {
+		return strings.ToUpper(name)
+	})
+	asset.AddFileString("index.html", "index.html content")
+	asset.AddFileString("asset1.html", "asset1.html content")
+
+	asset2 := assetmanager.New()
+	asset2.AddFileString("asset2.html", "asset2.html content")
+	asset.MergeAndRunReplacer(asset2)
+	c, _ := asset.GetString("ASSET2.HTML")
+	fmt.Println(c)
+	// Output: asset2.html content
+}
+func TestReplacer(t *testing.T) {
+
+	fileName := "test/index.html"
+	fileContent := "index.html content"
+
+	newFileName := "main::test/index.html"
+
+	asset := assetmanager.New()
+	// Add Replace Function
+	asset.AddReplacer("replacerTestName", replaceMe)
+
+	if asset.ExistsReplacer("replacerTestName") == false {
+		t.Error("AddReplacer Error")
+	}
+	asset.RemoveReplacer("replacerTestName")
+
+	if asset.ExistsReplacer("replacerTestName") == true {
+		t.Error("RemoveReplacer Error")
+	}
+
+	// Add another Replacer Function
+	asset.AddReplacer("testToMain", func(name string) string {
+		name = strings.Replace(name, "test/", "main::test/", -1)
+		return name
+	})
+
+	// Add File
+	asset.AddFileString(fileName, fileContent)
+	// Get it with the new name.
+	r, e := asset.GetString(newFileName)
+	if e != nil {
+		t.Error(e)
+	}
+	if r != fileContent {
+		t.Errorf("File content not equal %s", fileContent)
+	}
+}
+func TestExt(t *testing.T) {
+	asset := assetmanager.New()
+	asset.AddAllowedExt(".html", ".tpl")
+	asset.AddDisallowExt(".php", ".jpg")
+
+	ext1 := asset.GetExt("index.html")
+	ext2 := asset.GetExt("index.php")
+
+	if ext1 != ".html" {
+		t.Errorf("getExt Error ")
+	}
+	if !asset.CheckAllowed(ext1) {
+		t.Errorf("CheckAllowed Error ")
+	}
+	if asset.CheckAllowed(ext2) {
+		t.Errorf("CheckAllowed Error ")
+	}
+
+	if !asset.CheckDisallowed(ext1) {
+		t.Errorf("CheckDisallowed Error: CheckDisallowed ")
+	}
+	if asset.CheckDisallowed(ext2) {
+		t.Errorf("CheckDisallowed Error: CheckDisallowed ")
+	}
+
+	if asset.CheckAddFile("index.php") {
+		t.Errorf("canAddFile Error: canAddFile index.php")
+	}
+	if !asset.CheckAddFile("index.html") {
+		t.Errorf("canAddFile Error: canAddFile index.html")
+	}
+
+}
+
+func TestAddRemoveAllowed(t *testing.T) {
+	asset := assetmanager.New()
+	asset.AddAllowedExt(".html", ".tpl", ".php")
+
+	if asset.CheckAllowed(".html") != true {
+		t.Errorf("RemoveAllowedExt Error: CheckAllowed ")
+	}
+
+	asset.RemoveAllowedExt(".html")
+
+	if asset.CheckAllowed(".html") != false {
+		t.Errorf("RemoveAllowedExt Error: CheckAllowed ")
+	}
+}
+func TestAddRemoveDisallowed(t *testing.T) {
+	asset := assetmanager.New()
+	asset.AddDisallowExt(".html", ".tpl", ".php")
+	//t.Error(asset.DisallowedExt)
+	if asset.CheckDisallowed(".html") != false {
+		t.Errorf("RemoveDisallowExt Error: CheckAllowed ")
+	}
+	asset.RemoveDisallowExt(".html")
+	//t.Error(asset.DisallowedExt)
+	if asset.CheckDisallowed(".html") != true {
+		t.Errorf("RemoveDisallowExt Error: CheckAllowed ")
+	}
+}
+
+func TestAddFile(t *testing.T) {
+
+	asset := assetmanager.New()
+	file := "test/index.html"
+	asset.AddFile(file)
+	if !asset.Exists(file) {
+		t.Errorf("File Not Found %s", file)
+	}
+	content, err := asset.GetString(file)
+	if err != nil {
+		t.Error("Get File Error", err)
+	}
+	if content != "index.html" {
+		t.Error("Get File content not equal index.html")
+	}
+
+}
+
+func TestAddFileString(t *testing.T) {
+
+	asset := assetmanager.New()
+	file := "test/index.html"
+	content := "index.html"
+	asset.AddFileString(file, content)
+	if !asset.Exists(file) {
+		t.Errorf("File Not Found %s", file)
+	}
+	content, err := asset.GetString(file)
+	if err != nil {
+		t.Error("GetString File Error", err)
+	}
+	if content != "index.html" {
+		t.Error("GetString File content not equal index.html")
+	}
+
+}
+
+func TestAddDir(t *testing.T) {
+
+	asset := assetmanager.New()
+	asset.AddAllowedExt(".html", ".tpl")
+	asset.AddDisallowExt(".php", ".tpl")
+	//asset.AddDir(helpers.DirName())
+	asset.AddDir("./test")
+	//t.Error(asset.Files)
+	content, err := asset.Get("test/index.html")
+	if err != nil {
+		t.Error("Get File Error test/index.html ", err)
+	}
+	if string(content) != string([]byte("index.html")) {
+		t.Error("Get File content not equal index.html")
+	}
+
+	contentString, errString := asset.GetString("test/index.html")
+	if errString != nil {
+		t.Error("GetString Error test/index.html ", errString)
+	}
+	if contentString != "index.html" {
+		t.Error("Get File content not equal index.html")
+	}
+
+	r, e := asset.GetString("test/randomFile.html")
+	if e == nil {
+		t.Error("GetString need return error  ", e)
+	}
+	if r != "" {
+		t.Error("GetString need return empty")
+	}
+}
